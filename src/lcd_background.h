e79ec5829a09f84aad9152c901b185bb0aa7a0c7
elehobica <elehobica@gmail.com>
2024-06-02 13:45:15 +0000

    fix to extern C LCD_GetBackground()

--- src/lcd_background.h
+++ src/lcd_background.h
@@ -10,4 +10,14 @@
 
 #include "lcd_extra.h"
 
+// extern "C" is needed to overwrite (weak) LCD_GetBackground() in lcd_extra.c (pico_st7735_80x160 library)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 u16 LCD_GetBackground(u16 x, u16 y);
+
+#ifdef __cplusplus
+}
+#endif
