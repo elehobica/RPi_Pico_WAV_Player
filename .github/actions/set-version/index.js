e533f960325327b19430ce084f4b6a40bae62819
elehobica <elehobica@gmail.com>
2025-04-08 13:07:35 +0000

    use vercel/ncc for set-version action

--- .github/actions/set-version/index.js
+++ .github/actions/set-version/index.js
@@ -1,3 +1,11 @@
+/*-----------------------------------------------------------/
+/ index.js
+/------------------------------------------------------------/
+/ Copyright (c) 2025, Elehobica
+/ Released under the BSD-2-Clause
+/ refer to https://opensource.org/licenses/BSD-2-Clause
+/-----------------------------------------------------------*/
+
 const core = require('@actions/core');
 const fs = require('fs');
 
@@ -5,7 +13,9 @@ try {
   const targetFile = core.getInput('target', {required: true});
   let text = fs.readFileSync(targetFile, 'utf-8');
   let version_str = core.getInput('version_str', {required: true});
-  fs.writeFileSync(targetFile, text.replace(/("CFG_VERSION",\s+)"\d\.\d\.\d"/, `$1"${core.getInput('version_str')}"`));
+  let version_str_size = core.getInput('version_str_size', {required: true});
+  version_str = version_str.length > version_str_size ? version_str.slice(0, version_str_size - 1) : version_str;
+  fs.writeFileSync(targetFile, text.replace(/("CFG_VERSION",\s+)"\d\.\d\.\d"/, `$1"${version_str}"`));
 } catch (error) {
     core.setFailed(error.message);
 }
