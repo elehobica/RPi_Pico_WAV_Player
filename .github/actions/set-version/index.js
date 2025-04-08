cbc18107668ff2d6380e014cbf6f1c18d71f7feb
elehobica <elehobica@gmail.com>
2025-04-08 22:26:10 +0000

    introduce ref_max_length on generate-version-string step

--- .github/actions/set-version/index.js
+++ .github/actions/set-version/index.js
@@ -14,7 +14,7 @@ try {
   let text = fs.readFileSync(targetFile, 'utf-8');
   let version_str = core.getInput('version_str', {required: true});
   let version_str_size = core.getInput('version_str_size', {required: true});
-  version_str = version_str.length > version_str_size ? version_str.slice(0, version_str_size - 1) : version_str;
+  version_str = version_str.length > version_str_size ? version_str.slice(0, version_str_size) : version_str;
   fs.writeFileSync(targetFile, text.replace(/("CFG_VERSION",\s+)"\d\.\d\.\d"/, `$1"${version_str}"`));
 } catch (error) {
     core.setFailed(error.message);
